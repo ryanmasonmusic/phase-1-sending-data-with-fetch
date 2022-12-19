e6535fd9d69bdb19bc45a0691efacbcedffbbfa0
Ryan Mason <ryanmasonmusic@gmail.com>
2022-12-18 20:49:24 -0500

    commit tree 905c7d96a6a08a929ddd5167a382a6a453b100ff

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+function submitData(name, email){
+    const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+    name, email}),
+};
+return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object.id);
+        document.body.append(object.id);
+    })
+    .catch(function (error){
+        // alert('Unauthorized Access');
+        console.log("TESTING", error.message);
+        document.body.innerHTML = 'Unauthorized Access'
+    });
+};
